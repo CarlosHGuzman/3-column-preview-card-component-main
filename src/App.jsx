c6676592f6cb84883c204424f146e36bc115243e
CarlosHGuzman <89936497+CarlosHGuzman@users.noreply.github.com>
2022-11-30 15:33:47 -0500

    3-components by Carlos Guzmán

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import Card from "./components/Card"
 import db from "./db/textCards.json"
+import icon1 from "./assets/icon-sedans.svg"
+import icon2 from "./assets/icon-luxury.svg"
+import icon3 from "./assets/icon-suvs.svg"
+
 
 function App() {
   const colorsCard = [{backgroundColor:"hsl(31, 77%, 52%)"}, {backgroundColor:"hsl(184, 100%, 22%)"}, {backgroundColor:"hsl(179, 100%, 13%)"}]
 
   return (
-    <div className="App">
-      <div className="cards">
-        <Card db = {db["card-1"]} backgroundColor = {colorsCard[0]}/>
-        <Card db = {db["card-2"]} backgroundColor = {colorsCard[1]}/>
-        <Card db = {db["card-3"]} backgroundColor = {colorsCard[2]}/>
+    <div className="App" role="content-app">
+      <div className="cards" role="main">
+        <Card db = {db["card-1"]} backgroundColor = {colorsCard[0]} icon = {icon1}/>
+        <Card db = {db["card-2"]} backgroundColor = {colorsCard[1]} icon = {icon3}/>
+        <Card db = {db["card-3"]} backgroundColor = {colorsCard[2]} icon = {icon2}/>
       </div> 
-      <div className="attribution">
+      <div className="attribution" role="footer">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
         Coded by <a href="https://portafolio-carlos-guzman.netlify.app/">Carlos Guzmán</a>.
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,15 @@
 import useTextCard from "../customHooks/useTextCard"
-import icon from "../assets/icon-luxury.svg"
 import { useState } from "react"
 
-const Card = ({db, backgroundColor}) => {
+const Card = ({db, backgroundColor, icon}) => {
     const [colorBtn, setColorBtn] = useState({color: backgroundColor.backgroundColor})
     const [title, description] = useTextCard(db)
     const changeColorBtn = (valor) => {
         setColorBtn(valor&&{color: backgroundColor.backgroundColor})
     }
     return (
-        <article style={backgroundColor} className="card">
-            <img src={icon}></img>
+        <article style={backgroundColor} className="card" role={`card-${title}`}>
+            <img src={icon} alt="icon-car"></img>
             <h1 className="card-title"> {title}</h1>
             <p className="card-description"> {description}</p>
             <button onMouseUpCapture={() => {changeColorBtn(true)}} style={colorBtn}className="btn">Learn More</button>
